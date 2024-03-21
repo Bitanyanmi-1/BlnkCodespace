0ed3b2d8357f21ed268337ac400db6f747933fd3
N-nyiimock Bitanyanmi <118021911+Bitanyanmi-1@users.noreply.github.com>
2024-03-21 12:21:40 +0000

    Add code to print name, age, and city details

--- index.py
+++ index.py
@@ -23,4 +23,12 @@ def fibonacci(n):
             print(c)
 fibonacci(10)
 
+
+"""Declare and assign two variables with your name, age and city details
+ and print each of them with proper descriptions"""
+name = 'John Doe'
+age = 25
+city = 'New York'
+print(f'My name is {name}, I am {age} years old and I live in {city}')
+
 input('\n\nPress enter to exit\n\n')
