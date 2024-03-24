238cd1eaf5edfe092336cfaa600a326d4a6e32b3
N-nyiimock Bitanyanmi <118021911+Bitanyanmi-1@users.noreply.github.com>
2024-03-24 13:29:31 +0000

    Add dictionary creation and iteration, calculate quotient and remainder

--- index.py
+++ index.py
@@ -31,4 +31,26 @@ age = 25
 city = 'New York'
 print(f'My name is {name}, I am {age} years old and I live in {city}')
 
-input('\n\nPress enter to exit\n\n')
+
+#write a code to crete dictionary with 5 key value pairs
+dict = {
+    'name': 'John Doe',
+    'age': 25,
+    'city': 'New York',
+    'country': 'USA',
+    'occupation': 'Software Developer'
+}
+print(dict)
+
+#iterate through the dictionary and print the key value pairs
+for key, value in dict.items():
+    print(f'{key}: {value}')
+
+#Calculate and print the quotient and remainder when 10 is divided by 3
+quotient = 10 // 3
+remainder = 10 % 3
+print(f'\nQuotient: {quotient}, Remainder: {remainder}')
+print(f'{quotient}')
+
+    
+input('\n\nPress enter to exit...')
